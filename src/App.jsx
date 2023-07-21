71d90378220de02d8518e2ad55e7d4b61bd06a9e
Vyshnav Ajith <puthen1977@gmail.com>
2023-07-21 17:51:17 +0530

    Update App.jsx
    Edited the siteProps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Vyshnav Ajith",
+  title: "DevOps and Cloud Enthusiast",
+  email: "puthen1977@gmail.com",
+  gitHub: "vyshaj",
+  instagram: "vysh_aj",
+  linkedIn: "vyshnav-ajith",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
